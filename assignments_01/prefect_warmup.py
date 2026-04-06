50d57bb95bee6dd81b4947c85025afc897e11280
Daniel Desktop <danieladiazop@gmail.com>
2026-04-05 21:42:54 -0700

    reviewing prefect_warmup before submission

--- assignments_01/prefect_warmup.py
+++ assignments_01/prefect_warmup.py
@@ -2,10 +2,7 @@ from prefect import task, flow
 from prefect.logging import get_run_logger
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import statistics as stats
 from scipy import stats
-import seaborn as sns
 
 
 
@@ -15,28 +12,12 @@ arr = np.array([12.0, 15.0, np.nan, 14.0, 10.0, np.nan, 18.0, 14.0, 16.0, 22.0, 
 
 #Q2 Rebuild the pipeline from Q1 using Prefect. Do the following:
 
-"""  1. Turn the previous 3 functions into Prefect tasks using @task. 
-# 2. Turn data_pipeline() into a Prefect flow using @flow. 
-# 3. Call the 3 tasks in order and return the summary dictionary. Add this block at the bottom of the file so the flow runs when you execute the script.  """
-
-if __name__ == "__main__":
-    pipeline_flow()
-
-""" 
-4. Run the workflow from the terminal. Values should match that of Q1.
-5. Finally: Add a comment block at the bottom of prefect_warmup.py. Answer these 2 questions.
-
-    1. This pipeline is simple -- just three small functions on a handful of numbers. Why might Prefect be more overhead than it is worth here?
-
-    2. Describe some realistic scenarios where a framework like Prefect could still be useful, even if the pipeline logic itself stays simple like in this case.
-
- """
 
 
 #Define Tasks
 @task
 def create_series(arr):
-    values = pd.Series(arr)
+    values = pd.Series(arr, name="values")
     return values
 
 
@@ -61,12 +42,9 @@ def summarize_data(series):
 def data_pipeline(arr):
     logger = get_run_logger()
     values = create_series(arr)
-    clean_data = clean_data(values)
-    summary = summarize_data(clean_data)
+    cleaned = clean_data(values)
+    summary = summarize_data(cleaned)
     logger.info(summary)
-
-
-#print(data_pipeline(arr))
 
 
 #To run the flow
