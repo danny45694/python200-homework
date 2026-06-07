3c165aa20fb52bbbc9d41d08626fdb8ce7bb5de0
Daniel Desktop <danieladiazop@gmail.com>
2026-06-06 17:21:12 -0700

    started assignment_11

---
+++ assignments_11/warmup_11.py
@@ -0,0 +1,50 @@
+
+
+# -------------------------------- Prefect Orchestration --------------------------------
+
+
+# Q1
+
+"""
+
+1. Difference between a task and a flow like a computer. A task is a single component while a flow is the overall system/collective comprised of components(tasks).
+
+Tasks handle retries, caching, dependency tracking etc. Tasks should be simple and focused.
+
+Flow handles state tracking for the whole workflow and supports parameters, schedules and deployments.
+
+I would not decorate the Celsius to Fahrenheit helper function. Prefect is designed for things that may fail, like an API or querying a databse. They are more focused on I/O actions. Adding @task to simple, lightweight functions that perform quick calculations is counter-productive. 
+"""
+
+
+# Q2
+
+
+"""
+@task(retries=3, retry_delay_seconds=30)
+def call_api:
+"""
+
+#Q3
+
+
+"""
+
+To see what happened, you can click on the specific flow and the following page will provide detailed information about individual tasks and logs. 
+
+Here you can see Info, warning and error logs. If it failed in the transform step, first thing is to check the code in the transform step. I'd check if the input is correct, if correct, the error is isolated to the transform area. If input is incorrect, the extract location is where the bug resides. 
+
+I can narrow down from there. 
+
+"""
+
+
+# --------------------------------- Production Patterns -------------------------------
+
+
+# Q1
+
+"""
+
+raise_for_status surface errors cleanly. It stops the program from continuing when something goes wrong, makes you deal with the responses early, and makes error handling structured and consistent. 
+"""
